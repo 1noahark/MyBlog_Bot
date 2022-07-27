00ef14151a94c2311e955c3397222580affbc378
Noah Eyob <63798421+1noahark@users.noreply.github.com>
2022-07-27 15:57:20 -0200

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const blog = require("./model/model")
 
 app.set('port', (process.env.PORT || 5000));
 
+// damn express
+
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 
 // connect to DB
 
